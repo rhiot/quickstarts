19e98138350296ff14b14b9c0b25949c725fc2f4
Henryk Konsek <hekonsek@gmail.com>
2015-11-12 09:16:37 +0100

    Refactoring.

--- kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -2,12 +2,15 @@ package io.rhiot.quickstarts.kura.camel;
 
 import org.apache.camel.component.kura.KuraRouter;
 
+/**
+ * Example of the Kura Camel application.
+ */
 public class GatewayRouter extends KuraRouter {
 
     @Override
     public void configure() throws Exception {
-        from("timer://gateway-trigger").
-                to("log:gateway");
+        from("timer://heartbeat").
+                to("log:heartbeat");
     }
 
 }
