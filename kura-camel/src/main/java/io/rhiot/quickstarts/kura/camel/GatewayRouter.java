24c3fbb8133d2f7d0a4f1d23c2ec969d1bb24d2c
Henryk Konsek <hekonsek@gmail.com>
2015-11-19 13:03:51 +0100

    Migrated to RhiotKuraRouter.

--- kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -1,37 +1,19 @@
 package io.rhiot.quickstarts.kura.camel;
 
+import io.rhiot.component.kura.router.RhiotKuraRouter;
 import org.apache.camel.CamelContext;
-import org.apache.camel.component.kura.KuraRouter;
 import org.apache.camel.component.log.LogComponent;
 import org.apache.camel.component.timer.TimerComponent;
-import org.osgi.framework.BundleContext;
 
 /**
  * Example of the Kura Camel application.
  */
-public class GatewayRouter extends KuraRouter {
+public class GatewayRouter extends RhiotKuraRouter {
 
     @Override
     public void configure() throws Exception {
         from("timer://heartbeat").
                 to("log:heartbeat");
-    }
-
-    // TODO Remove this overriden method as soon as Camel 2.17 is out (see CAMEL-9314)
-    @Override
-    public void start(BundleContext bundleContext) throws Exception {
-        try {
-            super.start(bundleContext);
-        } catch (Throwable e) {
-            String errorMessage = "Problem when starting Kura module " + getClass().getName() + ":";
-            log.warn(errorMessage, e);
-
-            // Print error to the Kura console.
-            System.err.println(errorMessage);
-            e.printStackTrace();
-
-            throw e;
-        }
     }
 
     @Override
