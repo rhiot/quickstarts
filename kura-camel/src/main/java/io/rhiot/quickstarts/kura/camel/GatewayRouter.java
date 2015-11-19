2c6cfe5c3516203829ca6a2c4f154572d0640965
Henryk Konsek <hekonsek@gmail.com>
2015-11-19 13:12:46 +0100

    Added Kura router tests.

--- kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -3,6 +3,7 @@ package io.rhiot.quickstarts.kura.camel;
 import io.rhiot.component.kura.router.RhiotKuraRouter;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.log.LogComponent;
+import org.apache.camel.component.mock.MockComponent;
 import org.apache.camel.component.timer.TimerComponent;
 
 /**
@@ -13,13 +14,15 @@ public class GatewayRouter extends RhiotKuraRouter {
     @Override
     public void configure() throws Exception {
         from("timer://heartbeat").
-                to("log:heartbeat");
+                to("log:heartbeat").
+                to("mock:test");
     }
 
     @Override
     protected void beforeStart(CamelContext camelContext) {
         camelContext.addComponent("timer", new TimerComponent());
         camelContext.addComponent("log", new LogComponent());
+        camelContext.addComponent("mock", new MockComponent());
     }
 
 }
