d9f8faab32dc9b1b257a68c72482b5daf7e5d2db
gautric <gautric@redhat.com>
2016-02-16 15:49:24 +0100

    rebase

--- kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -1,29 +1,18 @@
 package io.rhiot.quickstarts.kura.camel;
 
 import io.rhiot.component.kura.router.RhiotKuraRouter;
-import org.apache.camel.CamelContext;
-import org.apache.camel.component.log.LogComponent;
-import org.apache.camel.component.mock.MockComponent;
-import org.apache.camel.component.timer.TimerComponent;
 
 /**
  * Example of the Kura Camel application.
  */
 public class GatewayRouter extends RhiotKuraRouter {
 
-    @Override
-    public void configure() throws Exception {
-        from("timer://heartbeat").
-                to("log:heartbeat").
-                to("mock:test");
-    }
-
-    @Override
-    protected void beforeStart(CamelContext camelContext) {
-        super.beforeStart(camelContext);
-        camelContext.addComponent("timer", new TimerComponent());
-        camelContext.addComponent("log", new LogComponent());
-        camelContext.addComponent("mock", new MockComponent());
-    }
+	@Override
+	public void configure() throws Exception {
+		
+		from("timer://io.rhiot.kura.timer?period={{metatype:camel.kura.gpio.ouput.period}}")
+				.to("log:io.rhiot.kura.heartbeat") 
+				.to("kura-gpio://{{metatype:camel.kura.gpio.ouput.id}}?action={{metatype:camel.kura.gpio.ouput.action}}");
+	}
 
 }
