6c3629cd1e7ef7de4c94387638845171f0ecd805
gautric <gautric@redhat.com>
2016-02-29 15:41:25 +0100

    template configuration

--- kura-camel-dio/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura-camel-dio/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -7,13 +7,14 @@ import io.rhiot.component.kura.router.RhiotKuraRouter;
  */
 public class GatewayRouter extends RhiotKuraRouter {
 
-    @Override
-    public void configure() throws Exception {
-        from("kura-gpio://{{metatype:camel.kura.gpio.input.id}}?direction=INPUT&mode=INPUT_PULL_DOWN&trigger={{metatype:camel.kura.gpio.input.trigger}}")
-                .to("deviceio-i2c:1/0x77?driver=bmp180").to("log:io.rhiot.kura.heartbeat")
-                .wireTap(
-                        "kura-gpio://{{metatype:camel.kura.gpio.output.id}}?action={{metatype:camel.kura.gpio.output.action}}")
-                .to("kura-cloud:demoAppId/tempMetric");
+	@Override
+	public void configure() throws Exception {
+		from("kura-gpio://{{metatype:camel.kura.gpio.input.id}}?direction=INPUT&mode=INPUT_PULL_DOWN&trigger={{metatype:camel.kura.gpio.input.trigger}}")
+				.to("deviceio-i2c://{{metatype:camel.kura.i2c.bus.id}}/{{metatype:camel.kura.i2c.device.id}}?driver={{metatype:camel.kura.i2c.device.driver}}")
+				.to("log:io.rhiot.kura.heartbeat")
+				.wireTap(
+						"kura-gpio://{{metatype:camel.kura.gpio.output.id}}?action={{metatype:camel.kura.gpio.output.action}}")
+				.to("kura-cloud:demoAppId/tempMetric");
 
-    }
+	}
 }
