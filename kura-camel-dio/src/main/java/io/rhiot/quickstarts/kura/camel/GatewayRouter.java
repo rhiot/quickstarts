ff32dce9c72fc7a028e77c7b17b30000fd47994c
gautric <gautric@redhat.com>
2016-03-08 12:42:16 +0100

    update dio quickstart

--- kura-camel-dio/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura-camel-dio/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -12,14 +12,15 @@ public class GatewayRouter extends RhiotKuraRouter {
         from("kura-gpio://{{metatype:camel.kura.gpio.input.id}}?direction=INPUT&mode=INPUT_PULL_DOWN&trigger={{metatype:camel.kura.gpio.input.trigger}}")
                 .id("[Magnetic-Route]").to("direct:mainRoute");
 
-        from("timer:tempRoute?period=30000&delay=1000").id("[Timer-Route]").to("direct:mainRoute");
+        from("timer:tempRoute?period=30000&delay=1000").id("[Timer-Route]")
+                .autoStartup("{{metatype:camel.kura.timer.startup:false}}").to("direct:mainRoute");
 
         from("direct:mainRoute").id("[Main-Route]")
                 .to("deviceio-i2c://{{metatype:camel.kura.i2c.bus.id}}/{{metatype:camel.kura.i2c.device.id}}?driver={{metatype:camel.kura.i2c.device.driver}}")
                 .to("log:io.rhiot.kura.heartbeat")
                 .wireTap(
                         "kura-gpio://{{metatype:camel.kura.gpio.output.id}}?action={{metatype:camel.kura.gpio.output.action}}")
-                .to("kura-cloud:demoAppId/tempMetric");
+                .to("kura-cloud:{{metatype:camel.kura.app.id}}/{{metatype:camel.kura.topic.id}}");
 
     }
 }
