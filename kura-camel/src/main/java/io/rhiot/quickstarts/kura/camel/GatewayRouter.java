21f8d5b6bddc11dc707c549dbdad383b33e50fb2
Henryk Konsek <hekonsek@gmail.com>
2016-04-13 11:19:04 +0200

    Added CamelKuraClient to the demo.

--- kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura-camel/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -1,6 +1,13 @@
 package io.rhiot.quickstarts.kura.camel;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.eclipse.kura.camel.camelcloud.DefaultCamelCloudService;
+import org.eclipse.kura.camel.cloud.KuraCloudComponent;
 import org.eclipse.kura.camel.router.CamelRouter;
+import org.eclipse.kura.message.KuraPayload;
+
+import java.util.Random;
 
 /**
  * Example of the Kura Camel application.
@@ -9,12 +16,25 @@ public class GatewayRouter extends CamelRouter {
 
     @Override
     public void configure() throws Exception {
+        KuraCloudComponent cloudComponent = new KuraCloudComponent();
+        cloudComponent.setCloudService(new DefaultCamelCloudService(camelContext));
+        camelContext.addComponent("kura-cloud", cloudComponent);
+
         from("timer://heartbeat").
-                setBody().simple("random(1,20)").
+                process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        KuraPayload payload = new KuraPayload();
+                        payload.addMetric("temperature", new Random().nextInt(20));
+                        exchange.getIn().setBody(payload);
+                    }
+                }).to("kura-cloud:myapp/topic");
+
+        from("kura-cloud:myapp/topic").
                 choice().
-                  when(simple("${body} < 10"))
+                  when(simple("${body.metrics()[temperature]} < 10"))
                 .to("log:lessThanTen")
-                  .when(simple("${body} == 10"))
+                  .when(simple("${body.metrics()[temperature]} == 10"))
                   .to("log:equalToTen")
                 .otherwise()
                 .to("log:greaterThanTen");
